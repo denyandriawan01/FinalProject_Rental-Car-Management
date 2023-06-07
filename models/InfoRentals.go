998612029169d63c85d0d64318dd872794186585
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-08 06:09:29 +0700

    Update InfoRentals.go

--- models/InfoRentals.go
+++ models/InfoRentals.go
@@ -2,12 +2,13 @@ package models
 
 type Rental struct {
 	RentalID    int64  `gorm:"primaryKey" json:"rental_id"`
-	UserID      int64  `gorm:"foreignKey:UserID" json:"user_id"`
+	UserID      int64  `gorm:"column:user_id" json:"user_id"`
 	CarID       int64  `gorm:"column:car_id" json:"car_id"`
 	UsageRegion string `json:"usage_region"`
 	RentalDate  string `json:"rental_date"`
 	ReturnDate  string `json:"return_date"`
 	TotalPrice  int64  `json:"total_price"`
 	IsCompleted bool   `json:"is_completed"`
-	Car   	    Car     `gorm:"foreignKey:CarID" json:"car"`
+	User	    User   `gorm:"foreignKey:UserID" json:"user_id"`
+	Car   	    Car    `gorm:"foreignKey:CarID" json:"car"`
 }
