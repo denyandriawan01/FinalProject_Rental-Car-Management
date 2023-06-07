b014cd8da885e68409c075f8a0552b944ce11b02
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-08 06:11:23 +0700

    Update InfoMaintenanceHistory.go

--- models/InfoMaintenanceHistory.go
+++ models/InfoMaintenanceHistory.go
@@ -2,9 +2,10 @@ package models
 
 type MaintenanceHistory struct {
 	MaintenanceID int64  `gorm:"primaryKey" json:"maintenance_id"`
-	CarID         int64  `gorm:"foreignKey:CarID" json:"car_id"`
+	CarID         int64  `gorm:"column:car_id" json:"car_id"`
 	LastOdometer  int64  `json:"last_odometer"`
 	Type          string `json:"type"`
 	Description   string `json:"description"`
 	Expense       int64  `json:"expense"`
+	Car	      Car    `gorm:"foreignKey:CarID" json:"car"`
 }
