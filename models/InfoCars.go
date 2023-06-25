7a7ef03ee58340f30b27f4b136b83e51835a9ee4
Deny Andriawan <deny.andriawan11@gmail.com>
2023-06-25 14:01:00 +0700

    Update cars model

--- models/InfoCars.go
+++ models/InfoCars.go
@@ -4,6 +4,8 @@ type Car struct {
 	CarID              int64  `gorm:"primaryKey" json:"car_id"`
 	Brand              string `gorm:"type:varchar(50)" json:"brand"`
 	Model              string `gorm:"type:varchar(50)" json:"model"`
+	Tipe               string `gorm:"type:varchar(50)" json:"Tipe"`
+	Capasity           string `gorm:"type:varchar(50)" json:"Capacity"`
 	Year               int64  `gorm:"int64" json:"year"`
 	RegistrationNumber string `gorm:"type:varchar(50)" json:"registration_number"`
 	VIN                string `gorm:"type:varchar(50)" json:"vin"`
